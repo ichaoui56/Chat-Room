a491cdc826f2523bf02752c8888a3ac4dc017e0f
ILYAS CHAOUI <144786151+ichaoui56@users.noreply.github.com>
2023-12-24 02:17:08 +0100

    edit my layout

--- views/_layout.php
+++ views/_layout.php
@@ -5,9 +5,14 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./assets/css/tailwind.css">
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
+    <link href="https://unpkg.com/movement.css/movement.css" rel="stylesheet">
     <link rel="icon" type="image/png" href="<?= PATH ?>assets/pictures/avito.png" />
     <title><?= ucfirst($page) ?></title>
-    <link rel="stylesheet" href="<?= PATH ?>assets/css/login.css">
+
+
+
 </head>
 <body>
 
@@ -17,5 +22,6 @@
 
     <footer></footer>
     <script src="<?= PATH ?>assets/js/main.js"></script>
+    <script src="<?= PATH ?>assets/js/home.js"></script>
 </body>
 </html>
