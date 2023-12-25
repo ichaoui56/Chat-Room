2ea4c687e1e83f54aaede8d83326fcbf1da85e91
ILYAS CHAOUI <144786151+ichaoui56@users.noreply.github.com>
2023-12-25 22:30:32 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,26 +2,28 @@
 
 include_once '_config/config.php';
 include_once '_functions/functions.php';
-include_once '_config/db.php';
-
 
 
 spl_autoload_register(function ($class) {
-    include_once '_classes/' . $class . '.php';
+    include_once 'models/' . $class . '.php';
 });
 
-if (isset($_GET['page']) && !empty($_GET['page'])) {
+$database = new Database();
+$con = $database->dbh;
+
+if (!empty($_GET['page'])) {
     $page = trim(strtolower($_GET['page']));
 } else {
     $page = 'home';
 }
 
 $all_pages = scandir('controllers');
-
 if (in_array($page . '_controller.php', $all_pages)) {
-    include_once 'models/' . $page . '_model.php';
+    //include_once 'models/Implementations/' . $page . 'Impl.php';
     include_once 'controllers/' . $page . '_controller.php';
     include_once 'views/_layout.php';
 } else {
-    header('Location: 404.html');
+    $page = '404';
+    include_once 'controllers/' . $page . '_controller.php';
+    include_once 'views/_layout.php';
 }
