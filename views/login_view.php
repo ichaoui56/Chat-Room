5f605328148bd7f925ef02ac025ba974f89641a3
ILYAS CHAOUI <144786151+ichaoui56@users.noreply.github.com>
2023-12-25 22:31:20 +0100

    Update login_view.php

--- views/login_view.php
+++ views/login_view.php
@@ -1,5 +1,6 @@
 
 <link rel="stylesheet" href="<?= PATH ?>assets/css/login.css">
+<title><?= ucfirst($page) ?></title>
 
 <input id="checkbox_toggle" type="checkbox" class="check">
 <div class="checkbox">
@@ -17,23 +18,22 @@
         <i style="--clr:#fffd44;"></i>
 
         <!-- login form  -->
-        <div id="loginForm" class="login">
-            <h2>Login</h2>
-            <div class="box-input">
-                <div class="border">
-                    <input type="email" name="email" class="input" placeholder="Email">
+        <form action="index.php?page=login"  id="loginForm" class="login" method="post">
+                <h2>Login</h2>
+                <div class="box-input">
+                    <div class="border">
+                        <input type="email" name="email" class="input" placeholder="Email">
+                    </div>
                 </div>
-            </div>
 
-            <div class="box-input">
-                <div class="border">
-                    <input type="password" name="password" class="input" placeholder="Password">
+                <div class="box-input">
+                    <div class="border">
+                        <input type="password" name="password" class="input" placeholder="Password">
+                    </div>
                 </div>
-            </div>
 
-            <button type="submit" class="animated-button"><span>SIGN IN</span></button>
-
-        </div>
+                <button type="submit" name="login" class="animated-button"><span>SIGN IN</span></button>
+        </form>
 
     </div>
 
@@ -43,51 +43,43 @@
         <i style="--clr:#fffd44;"></i>
 
         <!-- register form  -->
-        <div id="registerForm" class="login">
-            <h2>Register</h2>
+        <form action="index.php?page=login" id="registerForm" class="login" method="post" enctype="multipart/form-data">
+                <h2>Register</h2>
 
-            <div class="input-div">
-                <input class="upload" name="file" type="file">
-                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" stroke-linejoin="round"
-                     stroke-linecap="round" viewBox="0 0 24 24" stroke-width="2" fill="none" stroke="currentColor" class="icon">
-                    <polyline points="16 16 12 12 8 16"></polyline>
-                    <line y2="21" x2="12" y1="12" x1="12"></line>
-                    <path d="M20.39 18.39A5 5 0 0 0 18 9h-1.26A8 8 0 1 0 3 16.3"></path>
-                    <polyline points="16 16 12 12 8 16"></polyline>
-                </svg>
-            </div>
+                <div class="input-div">
+                    <input class="upload" name="picture" accept="image/*" type="file">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" stroke-linejoin="round"
+                         stroke-linecap="round" viewBox="0 0 24 24" stroke-width="2" fill="none" stroke="currentColor" class="icon">
+                        <polyline points="16 16 12 12 8 16"></polyline>
+                        <line y2="21" x2="12" y1="12" x1="12"></line>
+                        <path d="M20.39 18.39A5 5 0 0 0 18 9h-1.26A8 8 0 1 0 3 16.3"></path>
+                        <polyline points="16 16 12 12 8 16"></polyline>
+                    </svg>
+                </div>²
 
-            <div class="box-input">
-                <div class="border">
-                    <input type="email" name="email" class="input" placeholder="Email"
-                           pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$" title="Please enter a valid email address.">
+                <div class="box-input">
+                    <div class="border">
+                        <input type="text" name="username" class="input" placeholder="Username"
+                               pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$" title="Please enter a valid email address.">
+                    </div>
                 </div>
-            </div>
 
-            <div class="box-input">
-                <div class="border">
-                    <input type="email" name="email" class="input" placeholder="Email"
-                           pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$" title="Please enter a valid email address.">
+                <div class="box-input">
+                    <div class="border">
+                        <input type="email" name="email" class="input" placeholder="Email"
+                               pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$" title="Please enter a valid email address.">
+                    </div>
                 </div>
-            </div>
 
-            <div class="box-input">
-                <div class="border">
-                    <input type="password" name="password" class="input" placeholder="Password"
-                           pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
-                           title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters">
+                <div class="box-input">
+                    <div class="border">
+                        <input type="password" name="password" class="input" placeholder="Password">
+                    </div>
                 </div>
-            </div>
 
-            <div class="box-input">
-                <div class="border">
-                    <input type="password" name="cpassword" class="input" placeholder="Confirm Password ">
-                </div>
-            </div>
+                <button type="submit" name="signup" class="animated-button"><span>SIGN UP</span></button>
 
-            <button type="submit" class="animated-button"><span>SIGN UP</span></button>
-
-        </div>
+        </form>
 
     </div>
 </section>
