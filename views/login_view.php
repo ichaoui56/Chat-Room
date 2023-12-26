d813608ded36ffdf276f84cc315d3f93c116014c
ILYAS CHAOUI <144786151+ichaoui56@users.noreply.github.com>
2023-12-26 16:05:43 +0100

    Update login_view.php

--- views/login_view.php
+++ views/login_view.php
@@ -47,7 +47,7 @@
                 <h2>Register</h2>
 
                 <div class="input-div">
-                    <input class="upload" name="picture" accept="image/*" type="file">
+                    <input class="upload" id="picture" type="file" name="picture" accept="image/*" >
                     <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" stroke-linejoin="round"
                          stroke-linecap="round" viewBox="0 0 24 24" stroke-width="2" fill="none" stroke="currentColor" class="icon">
                         <polyline points="16 16 12 12 8 16"></polyline>
@@ -55,7 +55,7 @@
                         <path d="M20.39 18.39A5 5 0 0 0 18 9h-1.26A8 8 0 1 0 3 16.3"></path>
                         <polyline points="16 16 12 12 8 16"></polyline>
                     </svg>
-                </div>²
+                </div>
 
                 <div class="box-input">
                     <div class="border">
@@ -83,3 +83,4 @@
 
     </div>
 </section>
+<script src="<?= PATH ?>assets/js/main.js"></script>
