fc232eca8385af713e860592cb63071077029a01
ILYAS CHAOUI <144786151+ichaoui56@users.noreply.github.com>
2023-12-26 16:01:55 +0100

    add room traitment

--- controllers/discussion_controller.php
+++ controllers/discussion_controller.php
@@ -1,1 +1,17 @@
 <?php
+if (file_exists('../_config/db.php'))
+    include_once '../_config/db.php';
+if (file_exists('../models/Room.php')) {
+    include_once '../models/Room.php';
+}
+
+if (isset($_POST["roomName"])) {
+    extract($_POST);
+    $creator = $_SESSION["user_id"];
+    $room = new Room();
+    $room->insertRoom($roomName, $creator, $members, $db);
+    exit;
+}
+
+$users = User::getAll();
+
