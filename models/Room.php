3c017ca1cf5f400368ee26a80f8c65c3e3269984
ILYAS CHAOUI <144786151+ichaoui56@users.noreply.github.com>
2023-12-28 00:41:20 +0100

    add bind iisi

--- models/Room.php
+++ models/Room.php
@@ -5,7 +5,7 @@ class Room {
         $sql = "INSERT INTO room (room_name, creator) VALUES (?,?)";
         $stmt = mysqli_stmt_init($db);
         mysqli_stmt_prepare($stmt, $sql);
-        mysqli_stmt_bind_param($stmt, "si", $roomName, $creator);
+        mysqli_stmt_bind_param($stmt, "si", $roomName,$creator);
         mysqli_stmt_execute($stmt);
         $roomId = mysqli_insert_id($db);
 
@@ -16,6 +16,7 @@ class Room {
         mysqli_stmt_close($stmt);
         mysqli_close($db);
     }
+
     function insertMember($roomId, $member, $db) {
         $sql = "INSERT INTO room_member (room_id, user_id) VALUES (?,?)";
         $stmt = mysqli_stmt_init($db);
@@ -28,14 +29,14 @@ class Room {
         $sql = "INSERT INTO message (room_id, user_id, message, date) VALUES (?,?,?,?)";
         $stmt = mysqli_stmt_init($db);
         mysqli_stmt_prepare($stmt, $sql);
-        mysqli_stmt_bind_param($stmt, "ii", $roomId, $member, $message, $date);
+        mysqli_stmt_bind_param($stmt, "iisi", $roomId, $member, $message, $date);
         mysqli_stmt_execute($stmt);
     }
 
     static function getAll()
     {
         global $db;
-        $result = $db->query("SELECT * FROM room");
+        $result = $db->query("SELECT * FROM room ORDER BY `room_id` DESC");
         if ($result)
             return $result->fetch_all(MYSQLI_ASSOC);
     }
@@ -46,7 +47,7 @@ class Room {
         $result = $db->query("
             SELECT user.* FROM room_member
             JOIN user ON room_member.user_id = user.user_id
-            WHERE room_member.room_id = '$room_id'
+            WHERE room_member.room_id = '$room_id' 
         ");
 
         if ($result)
@@ -65,6 +66,7 @@ class Room {
         if ($result)
             return $result->fetch_all(MYSQLI_ASSOC);
     }
+
 }
 
 ?>
