cce121831ea8d1b0902a1db53e294620aed0f389
ILYAS CHAOUI <144786151+ichaoui56@users.noreply.github.com>
2023-12-28 21:47:36 +0100

    add addFriendRequest function

--- models/Room.php
+++ models/Room.php
@@ -67,6 +67,39 @@ class Room {
             return $result->fetch_all(MYSQLI_ASSOC);
     }
 
+    static function addFriendRequest($sender, $receiver) {
+        global $db;
+        $sql = "INSERT INTO friend_request (sender, receiver) VALUES (?,?)";
+        $stmt = mysqli_stmt_init($db);
+        mysqli_stmt_prepare($stmt, $sql);
+        mysqli_stmt_bind_param($stmt, "ii", $sender, $receiver);
+        mysqli_stmt_execute($stmt);
+    }
+
+    static function getFriendInvitation($receiver) {
+        global $db;
+        $result = $db->query("
+            SELECT user.* FROM friend_request
+            JOIN user ON friend_request.sender = user.user_id
+            WHERE message.room_id = '$receiver'
+        ");
+
+        if ($result)
+            return $result->fetch_all(MYSQLI_ASSOC);
+    }
+    static function invitaionDisplay($user, $db)
+    {
+        $result = $db->query("
+            SELECT room_invitation.invitation_id, room.*, user.username, user.picture FROM room_invitation
+            JOIN room ON room_invitation.room_id = room.room_id
+            JOIN user ON room_invitation.sender = user.user_id
+            WHERE room_invitation.receiver = '$user'
+        ");
+
+        if ($result)
+            return $result->fetch_all(MYSQLI_ASSOC);
+    }
+
 }
 
 ?>
